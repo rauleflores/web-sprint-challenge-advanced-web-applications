b14542127e9695e416b0796a54bc69eb4cce40cb
Raul Flores <raulefloresjr@gmail.com>
2020-07-17 18:14:08 -0700

    CRA done w/deps installed in root&client folders/rough login form done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+	const [formState, setFormState] = useState({
+		username: "",
+		password: "",
+		submitted: false,
+	});
+
+	const handleChanges = (e) => {
+		setFormState({
+			...formState,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const formSubmit = (e) => {
+		e.preventDefault();
+		setFormState({
+			username: "",
+			password: "",
+			submitted: false,
+		});
+	};
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+	return (
+		<>
+			<form onSubmit={formSubmit}>
+				<label htmlFor="username">Username</label>
+				<input
+					name="username"
+					value={formState.username}
+					placeholder="Please enter your username."
+					onChange={handleChanges}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					name="password"
+					value={formState.password}
+					placeholder="Please enter your password"
+					onChange={handleChanges}
+				/>
+				<button>Submit</button>
+			</form>
+		</>
+	);
 };
 
 export default Login;
